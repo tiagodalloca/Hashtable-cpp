74f24d31919c7d06c84908e7a13663174eabc394
MrDallOca <tdalloca@gmail.com>
2017-11-09 09:16:59 -0200

    Hashtable parameters set

--- header/Hashtable.h
+++ header/Hashtable.h
@@ -17,6 +17,14 @@ class Hashtable{
 	 bool remove(IHashble*);
 
  private:
+	 unsigned int length;
+	 unsigned int count;
+	 
+	 unsigned int growth_rate;
+	 unsigned int occupation_rate;
+	 unsigned int conflict_tree_max_size;
+	 unsigned int max_maximized_tree_count;
+	 
 	 Arvore<IHashble*>* the_array;
 };
 
